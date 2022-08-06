d7aef132823c42faa9e11b9ab678608f10d686ce
Marco Alonso <marco@garagecoders.net>
2022-08-06 14:02:33 -0500

    Mostrando lista de paises

--- CovidV2/EstadisticasViewController.swift
+++ CovidV2/EstadisticasViewController.swift
@@ -17,17 +17,39 @@ class EstadisticasViewController: UIViewController {
     @IBOutlet weak var totalConfirmados: UILabel!
     @IBOutlet weak var nuevosConfirmados: UILabel!
     
+    @IBOutlet weak var tablaPaises: UITableView!
+    
+    
     var covidManager = CovidManager()
+    var listaPaises: [CountriesStats] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         covidManager.delegado = self
+        tablaPaises.delegate = self
+        tablaPaises.dataSource = self
         
         covidManager.buscarEstadisticas()
     }
 
 
+}
+
+// MARK: - UITableView
+extension EstadisticasViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaPaises.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tablaPaises.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
+        celda.textLabel?.text = listaPaises[indexPath.row].Country
+        celda.detailTextLabel?.text = "Total de casos: \(listaPaises[indexPath.row].TotalConfirmed) "
+        return celda
+    }
+    
+    
 }
 
 extension EstadisticasViewController: covidManagerProtocol {
@@ -45,6 +67,10 @@ extension EstadisticasViewController: covidManagerProtocol {
             
             self.totalConfirmados.text = "Total de casos: \(totalConfirmadosStyle)"
             self.fecha.text = "Fecha: \(datos.Global.Date)"
+            
+            //TableView
+            self.listaPaises = datos.Countries
+            self.tablaPaises.reloadData()
         }
     }
     
