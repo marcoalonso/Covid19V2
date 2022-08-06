4fde24791b248afdd02418ea5dc04ed0d6d81ca4
Marco Alonso <marco@garagecoders.net>
2022-08-06 13:01:05 -0500

    Empezando

---
+++ CovidV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidV2
+//
+//  Created by marco rodriguez on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
